296db8e08eb9a004ab7d056f06e6ec60e1d5b2b1
Jonathan Roberto schapuis <121551546+jonathanKigo@users.noreply.github.com>
2023-03-09 14:10:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let count = 0;
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("DIMINUIR")) {
+      count--;
+    } else if (styles.contains("AUMENTAR")) {
+      count++;
+    } else {
+      count = 0;
+    }
+
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
+});
